4e43e72e7f2e7c2d48dbc25348b635eed0230629
Hugh Simpson <hugh.simpson@babylonhealth.com>
2021-08-10 16:56:47 +0100

    seems like the protobuf libraries exclude empty objects from serialization, so for the tests to pass we need to include at least one field for each value

--- protoshim/src/test/scala/com/babylonhealth/lit/protoshim/POCTest.scala
+++ protoshim/src/test/scala/com/babylonhealth/lit/protoshim/POCTest.scala
@@ -3,6 +3,7 @@ package com.babylonhealth.lit.protoshim
 import com.google.fhir.r4.core.{
   Canonical,
   CodeableConcept,
+  Decimal,
   Identifier,
   Meta,
   Observation,
@@ -10,6 +11,7 @@ import com.google.fhir.r4.core.{
   Period,
   Quantity,
   Reference,
+  ReferenceId,
   Uri,
   String => GPBString
 }
@@ -36,30 +38,33 @@ class POCTest extends AnyFreeSpec with Matchers {
       Bmi(
         identifier = LitSeq(LitIdentifier(system = Some("http://system.foo/bar"), value = Some("123"))),
         status = OBSERVATION_STATUS.FINAL,
-        subject = LitReference(),
-        category = LitSeq(LitCodeableConcept()),
-        value = LitQuantity(),
-        effective = choice(LitPeriod()),
-        code = LitCodeableConcept()
+        subject = LitReference(reference = Some("Dummy")),
+        category = LitSeq(LitCodeableConcept(text = Some("Abstract concept"))),
+        value = LitQuantity(value = Some(1.2)),
+        effective = choice(LitPeriod(id = Some("period-id"))),
+        code = LitCodeableConcept(text = Some("Concrete concept"))
       )
     val protoBMI: Observation = Observation
       .newBuilder()
       .setMeta(Meta
         .newBuilder()
-        .addProfile(Canonical.newBuilder().setValue("https://fhir.bbl.health/StructureDefinition/bmi").build()))
+        .addProfile(Canonical.newBuilder().setValue("https://fhir.bbl.health/StructureDefinition/bmi")))
       .addIdentifier(
         Identifier
           .newBuilder()
-          .setSystem(Uri.newBuilder().setValue("http://system.foo/bar").build())
-          .setValue(GPBString.newBuilder.setValue("123").build())
-          .build
+          .setSystem(Uri.newBuilder().setValue("http://system.foo/bar"))
+          .setValue(GPBString.newBuilder.setValue("123"))
       )
-      .setStatus(Observation.StatusCode.newBuilder().setValue(ObservationStatusCode.Value.FINAL).build())
-      .setSubject(Reference.newBuilder().build())
-      .addCategory(CodeableConcept.newBuilder().build())
-      .setValue(Observation.ValueX.newBuilder().setQuantity(Quantity.newBuilder().build()).build())
-      .setEffective(Observation.EffectiveX.newBuilder().setPeriod(Period.newBuilder().build()).build())
-      .setCode(CodeableConcept.newBuilder().build())
+      .setStatus(Observation.StatusCode.newBuilder().setValue(ObservationStatusCode.Value.FINAL))
+      .setSubject(Reference.newBuilder().setDocumentReferenceId(ReferenceId.newBuilder().setValue("Dummy")))
+      .addCategory(CodeableConcept.newBuilder().setText(GPBString.newBuilder().setValue("Abstract concept")))
+      .setValue(Observation.ValueX
+        .newBuilder()
+        .setQuantity(Quantity.newBuilder().setValue(Decimal.newBuilder().setValue("1.2"))))
+      .setEffective(Observation.EffectiveX
+        .newBuilder()
+        .setPeriod(Period.newBuilder().setId(GPBString.newBuilder().setValue("period-id"))))
+      .setCode(CodeableConcept.newBuilder().setText(GPBString.newBuilder().setValue("Concrete concept")))
       .build()
     "json from protopatient" in {
       Translator.printer.print(protoBMI)
@@ -70,6 +75,7 @@ class POCTest extends AnyFreeSpec with Matchers {
     "decodes from proto Message" in {
       val msg     = Translator.resourceToMessage(bmi)
       val decoded = Translator.resourceFromMessage(msg)
+      decoded.left.foreach(l => println(s"FAILED THIS: ${l}"))
       decoded.isRight shouldEqual true
       decoded.right.get shouldEqual bmi
     }
@@ -80,6 +86,7 @@ class POCTest extends AnyFreeSpec with Matchers {
     "decodes from Array[Byte]" in {
       val ser: Array[Byte]                          = Translator.resourceToBytes(bmi)
       val decoded: Either[circe.Error, LitResource] = Translator.resourceFromBytes(ser)
+      decoded.left.foreach(l => println(s"FAILED THAT: ${l}"))
       decoded.isRight shouldEqual true
       decoded.right.get shouldEqual bmi
     }
