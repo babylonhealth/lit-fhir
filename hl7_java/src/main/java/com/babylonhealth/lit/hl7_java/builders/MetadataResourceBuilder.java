928b0d4f090c2b71b95507dab2f6fceeb7a1d14b
Hugh Simpson <hugh.simpson@babylonhealth.com>
2021-08-24 18:59:12 +0100

    compile

--- hl7_java/src/main/java/com/babylonhealth/lit/hl7_java/builders/MetadataResourceBuilder.java
+++ hl7_java/src/main/java/com/babylonhealth/lit/hl7_java/builders/MetadataResourceBuilder.java
@@ -34,6 +34,7 @@ import com.babylonhealth.lit.core_java.builders.*;
 import com.babylonhealth.lit.hl7_java.builders.*;
 import com.babylonhealth.lit.core_java.model.Unions.*;
 import com.babylonhealth.lit.hl7_java.model.Unions.*;
+import com.babylonhealth.lit.hl7.PUBLICATION_STATUS;
 import com.babylonhealth.lit.core.LANGUAGES;
 import com.babylonhealth.lit.core.$bslash$div;
 import com.babylonhealth.lit.core_java.LitUtils;
@@ -45,26 +46,41 @@ import static java.util.stream.Collectors.toList;
 public interface MetadataResourceBuilder extends CanonicalResourceBuilder {
   public MetadataResource build();
 
-  public static Impl init() {
-    return new Impl();
+  public static Impl init(PUBLICATION_STATUS status) {
+    return new Impl(status);
   }
 
-  public static Impl builder() {
-    return new Impl();
+  public static Impl builder(PUBLICATION_STATUS status) {
+    return new Impl(status);
   }
 
   public class Impl implements MetadataResourceBuilder {
     private Optional<String> id = Optional.empty();
+    private Optional<String> url = Optional.empty();
     private Optional<Meta> meta = Optional.empty();
     private Optional<Narrative> text = Optional.empty();
+    private Optional<String> name = Optional.empty();
+    private Optional<FHIRDateTime> date = Optional.empty();
+    private Optional<String> title = Optional.empty();
     private Collection<CodeableConcept> topic = Collections.emptyList();
+    private PUBLICATION_STATUS status;
     private Collection<ContactDetail> author = Collections.emptyList();
     private Collection<ContactDetail> editor = Collections.emptyList();
+    private Optional<String> version = Optional.empty();
+    private Collection<ContactDetail> contact = Collections.emptyList();
+    private Optional<String> purpose = Optional.empty();
     private Optional<LANGUAGES> language = Optional.empty();
     private Collection<ContactDetail> reviewer = Collections.emptyList();
     private Collection<ContactDetail> endorser = Collections.emptyList();
+    private Optional<String> copyright = Optional.empty();
     private Collection<Resource> contained = Collections.emptyList();
+    private Optional<String> publisher = Optional.empty();
     private Collection<Extension> extension = Collections.emptyList();
+    private Collection<Identifier> identifier = Collections.emptyList();
+    private Collection<UsageContext> useContext = Collections.emptyList();
+    private Optional<String> description = Optional.empty();
+    private Collection<CodeableConcept> jurisdiction = Collections.emptyList();
+    private Optional<Boolean> experimental = Optional.empty();
     private Optional<FHIRDate> approvalDate = Optional.empty();
     private Optional<String> implicitRules = Optional.empty();
     private Optional<FHIRDate> lastReviewDate = Optional.empty();
@@ -72,8 +88,14 @@ public interface MetadataResourceBuilder extends CanonicalResourceBuilder {
     private Collection<RelatedArtifact> relatedArtifact = Collections.emptyList();
     private Collection<Extension> modifierExtension = Collections.emptyList();
 
-    /** Required fields for {@link MetadataResource} */
-    public Impl() {}
+    /**
+     * Required fields for {@link MetadataResource}
+     *
+     * @param status - The status of this {{title}}. Enables tracking the life-cycle of the content.
+     */
+    public Impl(PUBLICATION_STATUS status) {
+      this.status = status;
+    }
 
     /**
      * @param id - The logical id of the resource, as used in the URL for the resource. Once
@@ -81,6 +103,18 @@ public interface MetadataResourceBuilder extends CanonicalResourceBuilder {
      */
     public MetadataResourceBuilder.Impl withId(@NonNull String id) {
       this.id = Optional.of(id);
+      return this;
+    }
+    /**
+     * @param url - An absolute URI that is used to identify this {{title}} when it is referenced in
+     *     a specification, model, design or an instance; also called its canonical identifier. This
+     *     SHOULD be globally unique and SHOULD be a literal address at which at which an
+     *     authoritative instance of this {{title}} is (or will be) published. This URL can be the
+     *     target of a canonical reference. It SHALL remain the same when the {{title}} is stored on
+     *     different servers.
+     */
+    public MetadataResourceBuilder.Impl withUrl(@NonNull String url) {
+      this.url = Optional.of(url);
       return this;
     }
     /**
@@ -111,6 +145,29 @@ public interface MetadataResourceBuilder extends CanonicalResourceBuilder {
 
     public MetadataResourceBuilder.Impl withText(@NonNull NarrativeBuilder text) {
       this.text = Optional.of(text.build());
+      return this;
+    }
+    /**
+     * @param name - A natural language name identifying the {{title}}. This name should be usable
+     *     as an identifier for the module by machine processing applications such as code
+     *     generation.
+     */
+    public MetadataResourceBuilder.Impl withName(@NonNull String name) {
+      this.name = Optional.of(name);
+      return this;
+    }
+    /**
+     * @param date - The date (and optionally time) when the {{title}} was published. The date must
+     *     change when the business version changes and it must change if the status code changes.
+     *     In addition, it should change when the substantive content of the {{title}} changes.
+     */
+    public MetadataResourceBuilder.Impl withDate(@NonNull FHIRDateTime date) {
+      this.date = Optional.of(date);
+      return this;
+    }
+    /** @param title - A short, descriptive, user-friendly title for the {{title}}. */
+    public MetadataResourceBuilder.Impl withTitle(@NonNull String title) {
+      this.title = Optional.of(title);
       return this;
     }
     /**
@@ -176,6 +233,46 @@ public interface MetadataResourceBuilder extends CanonicalResourceBuilder {
       this.editor = Arrays.stream(editor).map(e -> e.build()).collect(toList());
       return this;
     }
+    /**
+     * @param version - The identifier that is used to identify this version of the {{title}} when
+     *     it is referenced in a specification, model, design or instance. This is an arbitrary
+     *     value managed by the {{title}} author and is not expected to be globally unique. For
+     *     example, it might be a timestamp (e.g. yyyymmdd) if a managed version is not available.
+     *     There is also no expectation that versions can be placed in a lexicographical sequence.
+     */
+    public MetadataResourceBuilder.Impl withVersion(@NonNull String version) {
+      this.version = Optional.of(version);
+      return this;
+    }
+    /**
+     * @param contact - Contact details to assist a user in finding and communicating with the
+     *     publisher.
+     */
+    public MetadataResourceBuilder.Impl withContact(@NonNull ContactDetail... contact) {
+      this.contact = Arrays.asList(contact);
+      return this;
+    }
+    /**
+     * @param contact - Contact details to assist a user in finding and communicating with the
+     *     publisher.
+     */
+    public MetadataResourceBuilder.Impl withContact(@NonNull Collection<ContactDetail> contact) {
+      this.contact = Collections.unmodifiableCollection(contact);
+      return this;
+    }
+
+    public MetadataResourceBuilder.Impl withContact(@NonNull ContactDetailBuilder... contact) {
+      this.contact = Arrays.stream(contact).map(e -> e.build()).collect(toList());
+      return this;
+    }
+    /**
+     * @param purpose - Explanation of why this {{title}} is needed and why it has been designed as
+     *     it has.
+     */
+    public MetadataResourceBuilder.Impl withPurpose(@NonNull String purpose) {
+      this.purpose = Optional.of(purpose);
+      return this;
+    }
     /** @param language - The base language in which the resource is written. */
     public MetadataResourceBuilder.Impl withLanguage(@NonNull LANGUAGES language) {
       this.language = Optional.of(language);
@@ -224,6 +321,15 @@ public interface MetadataResourceBuilder extends CanonicalResourceBuilder {
       return this;
     }
     /**
+     * @param copyright - A copyright statement relating to the {{title}} and/or its contents.
+     *     Copyright statements are generally legal restrictions on the use and publishing of the
+     *     {{title}}.
+     */
+    public MetadataResourceBuilder.Impl withCopyright(@NonNull String copyright) {
+      this.copyright = Optional.of(copyright);
+      return this;
+    }
+    /**
      * @param contained - These resources do not have an independent existence apart from the
      *     resource that contains them - they cannot be identified independently, nor can they have
      *     their own independent transaction scope.
@@ -244,6 +350,13 @@ public interface MetadataResourceBuilder extends CanonicalResourceBuilder {
 
     public MetadataResourceBuilder.Impl withContained(@NonNull ResourceBuilder... contained) {
       this.contained = Arrays.stream(contained).map(e -> e.build()).collect(toList());
+      return this;
+    }
+    /**
+     * @param publisher - The name of the organization or individual that published the {{title}}.
+     */
+    public MetadataResourceBuilder.Impl withPublisher(@NonNull String publisher) {
+      this.publisher = Optional.of(publisher);
       return this;
     }
     /**
@@ -271,6 +384,95 @@ public interface MetadataResourceBuilder extends CanonicalResourceBuilder {
 
     public MetadataResourceBuilder.Impl withExtension(@NonNull ExtensionBuilder... extension) {
       this.extension = Arrays.stream(extension).map(e -> e.build()).collect(toList());
+      return this;
+    }
+    /**
+     * @param identifier - A formal identifier that is used to identify this {{title}} when it is
+     *     represented in other formats, or referenced in a specification, model, design or an
+     *     instance.
+     */
+    public MetadataResourceBuilder.Impl withIdentifier(@NonNull Identifier... identifier) {
+      this.identifier = Arrays.asList(identifier);
+      return this;
+    }
+    /**
+     * @param identifier - A formal identifier that is used to identify this {{title}} when it is
+     *     represented in other formats, or referenced in a specification, model, design or an
+     *     instance.
+     */
+    public MetadataResourceBuilder.Impl withIdentifier(@NonNull Collection<Identifier> identifier) {
+      this.identifier = Collections.unmodifiableCollection(identifier);
+      return this;
+    }
+
+    public MetadataResourceBuilder.Impl withIdentifier(@NonNull IdentifierBuilder... identifier) {
+      this.identifier = Arrays.stream(identifier).map(e -> e.build()).collect(toList());
+      return this;
+    }
+    /**
+     * @param useContext - The content was developed with a focus and intent of supporting the
+     *     contexts that are listed. These contexts may be general categories (gender, age, ...) or
+     *     may be references to specific programs (insurance plans, studies, ...) and may be used to
+     *     assist with indexing and searching for appropriate {{title}} instances.
+     */
+    public MetadataResourceBuilder.Impl withUseContext(@NonNull UsageContext... useContext) {
+      this.useContext = Arrays.asList(useContext);
+      return this;
+    }
+    /**
+     * @param useContext - The content was developed with a focus and intent of supporting the
+     *     contexts that are listed. These contexts may be general categories (gender, age, ...) or
+     *     may be references to specific programs (insurance plans, studies, ...) and may be used to
+     *     assist with indexing and searching for appropriate {{title}} instances.
+     */
+    public MetadataResourceBuilder.Impl withUseContext(
+        @NonNull Collection<UsageContext> useContext) {
+      this.useContext = Collections.unmodifiableCollection(useContext);
+      return this;
+    }
+
+    public MetadataResourceBuilder.Impl withUseContext(@NonNull UsageContextBuilder... useContext) {
+      this.useContext = Arrays.stream(useContext).map(e -> e.build()).collect(toList());
+      return this;
+    }
+    /**
+     * @param description - A free text natural language description of the {{title}} from a
+     *     consumer's perspective.
+     */
+    public MetadataResourceBuilder.Impl withDescription(@NonNull String description) {
+      this.description = Optional.of(description);
+      return this;
+    }
+    /**
+     * @param jurisdiction - A legal or geographic region in which the {{title}} is intended to be
+     *     used.
+     */
+    public MetadataResourceBuilder.Impl withJurisdiction(@NonNull CodeableConcept... jurisdiction) {
+      this.jurisdiction = Arrays.asList(jurisdiction);
+      return this;
+    }
+    /**
+     * @param jurisdiction - A legal or geographic region in which the {{title}} is intended to be
+     *     used.
+     */
+    public MetadataResourceBuilder.Impl withJurisdiction(
+        @NonNull Collection<CodeableConcept> jurisdiction) {
+      this.jurisdiction = Collections.unmodifiableCollection(jurisdiction);
+      return this;
+    }
+
+    public MetadataResourceBuilder.Impl withJurisdiction(
+        @NonNull CodeableConceptBuilder... jurisdiction) {
+      this.jurisdiction = Arrays.stream(jurisdiction).map(e -> e.build()).collect(toList());
+      return this;
+    }
+    /**
+     * @param experimental - A Boolean value to indicate that this {{title}} is authored for testing
+     *     purposes (or education/evaluation/marketing) and is not intended to be used for genuine
+     *     usage.
+     */
+    public MetadataResourceBuilder.Impl withExperimental(@NonNull Boolean experimental) {
+      this.experimental = Optional.of(experimental);
       return this;
     }
     /**
@@ -389,16 +591,31 @@ public interface MetadataResourceBuilder extends CanonicalResourceBuilder {
     public MetadataResource build() {
       return new MetadataResource(
           OptionConverters.toScala(id),
+          OptionConverters.toScala(url),
           OptionConverters.toScala(meta),
           OptionConverters.toScala(text),
+          OptionConverters.toScala(name),
+          OptionConverters.toScala(date),
+          OptionConverters.toScala(title),
           topic.stream().collect(new LitSeqJCollector<>()),
+          status,
           author.stream().collect(new LitSeqJCollector<>()),
           editor.stream().collect(new LitSeqJCollector<>()),
+          OptionConverters.toScala(version),
+          contact.stream().collect(new LitSeqJCollector<>()),
+          OptionConverters.toScala(purpose),
           OptionConverters.toScala(language),
           reviewer.stream().collect(new LitSeqJCollector<>()),
           endorser.stream().collect(new LitSeqJCollector<>()),
+          OptionConverters.toScala(copyright),
           contained.stream().collect(new LitSeqJCollector<>()),
+          OptionConverters.toScala(publisher),
           extension.stream().collect(new LitSeqJCollector<>()),
+          identifier.stream().collect(new LitSeqJCollector<>()),
+          useContext.stream().collect(new LitSeqJCollector<>()),
+          OptionConverters.toScala(description),
+          jurisdiction.stream().collect(new LitSeqJCollector<>()),
+          OptionConverters.toScala(experimental.map(x -> (Object) x)),
           OptionConverters.toScala(approvalDate),
           OptionConverters.toScala(implicitRules),
           OptionConverters.toScala(lastReviewDate),
