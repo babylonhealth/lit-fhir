664d8cb9bb8d7e397fac29df32d0f07f2598ae30
Hugh Simpson <hugh.simpson@babylonhealth.com>
2021-10-04 11:33:59 +0100

    retain micros

--- core/src/main/scala/com/babylonhealth/lit/core/BaseFieldDecoders.scala
+++ core/src/main/scala/com/babylonhealth/lit/core/BaseFieldDecoders.scala
@@ -98,7 +98,11 @@ trait BaseFieldDecoders extends Utils {
         Try(FHIRDateTime(ZonedDateTime.of(y.toInt, m.toInt, 1, 0, 0, 0, 0, ZoneOffset.UTC), FHIRDateTimeSpecificity.Month))
       case yearsMonthsAndDays(y, m, d) =>
         Try(FHIRDateTime(ZonedDateTime.of(y.toInt, m.toInt, d.toInt, 0, 0, 0, 0, ZoneOffset.UTC), FHIRDateTimeSpecificity.Day))
-      case x => Try(FHIRDateTime(ZonedDateTime.parse(x), FHIRDateTimeSpecificity.Time))
+      case x => Try {
+        val time = ZonedDateTime.parse(x)
+        val specificity = if (time.getNano % 100000 == 0) FHIRDateTimeSpecificity.Time else FHIRDateTimeSpecificity.Micros
+        FHIRDateTime(time, specificity)
+      }
     }
   implicit val encoderFHIRDateTime: Encoder[FHIRDateTime] = Encoder.encodeString.contramap(_.fmt)
 
