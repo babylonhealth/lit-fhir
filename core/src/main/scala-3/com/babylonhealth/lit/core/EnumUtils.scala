f4ae25e8c4224af4c3b214bf2c360305abba38f3
Hugh Simpson <hugh.simpson@babylonhealth.com>
2022-09-24 23:09:33 +0100

    more compatibile enum interface with scala 2 versions

--- core/src/main/scala-3/com/babylonhealth/lit/core/EnumUtils.scala
+++ core/src/main/scala-3/com/babylonhealth/lit/core/EnumUtils.scala
@@ -16,7 +16,7 @@ trait ToCodingAble extends EnumBase {
 
 trait EType[A <: ToCodingAble](val reference: String) {
   def values: Array[A]
-  lazy val withName: String => A   = values.map(x => x.name -> x).toMap
+  def withName: String => A        = namesToValuesMap.apply
   implicit val encoder: Encoder[A] = Encoder.instance[A](Json fromString _.name)
   implicit val decoder: Decoder[A] =
     Decoder.instance[A](c =>
@@ -28,6 +28,20 @@ trait EType[A <: ToCodingAble](val reference: String) {
               else ""
             DecodingFailure(s"'$s' is not in $reference.$options", c.history)
           }))
+
+  lazy val namesToValuesMap: Map[String, A] = values.map(v => v.name -> v).toMap
+  lazy final val lowerCaseNamesToValuesMap: Map[String, A] =
+    namesToValuesMap.map { case (k, v) => k.toLowerCase -> v }
+  def withNameInsensitiveOption(name: String): Option[A] =
+    lowerCaseNamesToValuesMap.get(name.toLowerCase)
+
+  def withNameInsensitive(name: String): A =
+    withNameInsensitiveOption(name).getOrElse {
+      val options =
+        if (values.size <= 10) s" Valid values are: '${values.map(_.name).mkString(", ")}'."
+        else ""
+      throw new NoSuchElementException(s"'$name' is not in $reference.$options")
+    }
 }
 
 trait ETypeWithFallback[A <: ToCodingAble](implicit aClassTag: ClassTag[A]) extends EType[A] {
@@ -41,5 +55,8 @@ trait ETypeWithFallback[A <: ToCodingAble](implicit aClassTag: ClassTag[A]) exte
       .collect { case Success(v) => v }
       .toArray
   def fallback(s: String): A
-  override lazy val withName: String => A = values.map(x => x.name -> x).toMap.withDefault(fallback)
+  override lazy val namesToValuesMap: Map[String, A] = values.map(x => x.name -> x).toMap.withDefault(fallback)
+
+  override def withNameInsensitiveOption(name: String): Option[A] = Some(withName(name))
+  override def withNameInsensitive(name: String): A               = withName(name)
 }
