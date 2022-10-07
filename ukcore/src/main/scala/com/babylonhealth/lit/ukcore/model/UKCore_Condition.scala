9aed4c79e5d6392d24b5f01aaffed865b4fda6b8
hughsimpson <hugh.simpson@babylonhealth.com>
2022-10-07 09:34:47 +0100

    bump generator and re-run it. Standardises ukcore comment formatting (#192)

--- ukcore/src/main/scala/com/babylonhealth/lit/ukcore/model/UKCore_Condition.scala
+++ ukcore/src/main/scala/com/babylonhealth/lit/ukcore/model/UKCore_Condition.scala
@@ -263,77 +263,77 @@ object UKCore_Condition extends CompanionFor[UKCore_Condition] {
   * @constructor
   *   Inherits all params from parent.
   * @param id
-  *   \- The logical id of the resource, as used in the URL for the resource. Once assigned, this value never changes.
+  *   - The logical id of the resource, as used in the URL for the resource. Once assigned, this value never changes.
   * @param meta
-  *   \- The metadata about the resource. This is content that is maintained by the infrastructure. Changes to the content might
-  *   not always be associated with version changes to the resource.
+  *   - The metadata about the resource. This is content that is maintained by the infrastructure. Changes to the content might
+  *     not always be associated with version changes to the resource.
   * @param text
-  *   \- A human-readable narrative that contains a summary of the resource and can be used to represent the content of the
-  *   resource to a human. The narrative need not encode all the structured data, but is required to contain sufficient detail to
-  *   make it "clinically safe" for a human to just read the narrative. Resource definitions may define what content should be
-  *   represented in the narrative to ensure clinical safety.
+  *   - A human-readable narrative that contains a summary of the resource and can be used to represent the content of the
+  *     resource to a human. The narrative need not encode all the structured data, but is required to contain sufficient detail
+  *     to make it "clinically safe" for a human to just read the narrative. Resource definitions may define what content should
+  *     be represented in the narrative to ensure clinical safety.
   * @param subject
-  *   \- Indicates the patient or group who the condition record is associated with.
+  *   - Indicates the patient or group who the condition record is associated with.
   * @param language
-  *   \- The base language in which the resource is written.
+  *   - The base language in which the resource is written.
   * @param category
-  *   \- A category assigned to the condition.
+  *   - A category assigned to the condition.
   * @param severity
-  *   \- A subjective assessment of the severity of the condition as evaluated by the clinician.
+  *   - A subjective assessment of the severity of the condition as evaluated by the clinician.
   * @param onset
-  *   \- Estimated or actual date or date-time the condition began, in the opinion of the clinician.
+  *   - Estimated or actual date or date-time the condition began, in the opinion of the clinician.
   * @param recorder
-  *   \- Individual who recorded the record and takes responsibility for its content.
+  *   - Individual who recorded the record and takes responsibility for its content.
   * @param asserter
-  *   \- Individual who is making the condition statement.
+  *   - Individual who is making the condition statement.
   * @param contained
-  *   \- These resources do not have an independent existence apart from the resource that contains them - they cannot be
-  *   identified independently, and nor can they have their own independent transaction scope.
+  *   - These resources do not have an independent existence apart from the resource that contains them - they cannot be
+  *     identified independently, and nor can they have their own independent transaction scope.
   * @param extension
-  *   \- May be used to represent additional information that is not part of the basic definition of the resource. To make the use
-  *   of extensions safe and manageable, there is a strict set of governance applied to the definition and use of extensions.
-  *   Though any implementer can define an extension, there is a set of requirements that SHALL be met as part of the definition
-  *   of the extension.
+  *   - May be used to represent additional information that is not part of the basic definition of the resource. To make the use
+  *     of extensions safe and manageable, there is a strict set of governance applied to the definition and use of extensions.
+  *     Though any implementer can define an extension, there is a set of requirements that SHALL be met as part of the definition
+  *     of the extension.
   * @param encounter
-  *   \- The Encounter during which this Condition was created or to which the creation of this record is tightly associated.
+  *   - The Encounter during which this Condition was created or to which the creation of this record is tightly associated.
   * @param abatement
-  *   \- The date or estimated date that the condition resolved or went into remission. This is called "abatement" because of the
-  *   many overloaded connotations associated with "remission" or "resolution" - Conditions are never really resolved, but they
-  *   can abate.
+  *   - The date or estimated date that the condition resolved or went into remission. This is called "abatement" because of the
+  *     many overloaded connotations associated with "remission" or "resolution" - Conditions are never really resolved, but they
+  *     can abate.
   * @param recordedDate
-  *   \- The recordedDate represents when this particular Condition record was created in the system, which is often a
-  *   system-generated date.
+  *   - The recordedDate represents when this particular Condition record was created in the system, which is often a
+  *     system-generated date.
   * @param implicitRules
-  *   \- A reference to a set of rules that were followed when the resource was constructed, and which must be understood when
-  *   processing the content. Often, this is a reference to an implementation guide that defines the special rules along with
-  *   other profiles etc.
+  *   - A reference to a set of rules that were followed when the resource was constructed, and which must be understood when
+  *     processing the content. Often, this is a reference to an implementation guide that defines the special rules along with
+  *     other profiles etc.
   * @param clinicalStatus
-  *   \- The clinical status of the condition.
+  *   - The clinical status of the condition.
   * @param code
-  *   \- Identification of the condition, problem or diagnosis.
+  *   - Identification of the condition, problem or diagnosis.
   * @param note
-  *   \- Additional information about the Condition. This is a general notes/comments entry for description of the Condition, its
-  *   diagnosis and prognosis.
+  *   - Additional information about the Condition. This is a general notes/comments entry for description of the Condition, its
+  *     diagnosis and prognosis.
   * @param modifierExtension
-  *   \- May be used to represent additional information that is not part of the basic definition of the resource and that
-  *   modifies the understanding of the element that contains it and/or the understanding of the containing element's descendants.
-  *   Usually modifier elements provide negation or qualification. To make the use of extensions safe and manageable, there is a
-  *   strict set of governance applied to the definition and use of extensions. Though any implementer is allowed to define an
-  *   extension, there is a set of requirements that SHALL be met as part of the definition of the extension. Applications
-  *   processing a resource are required to check for modifier extensions. Modifier extensions SHALL NOT change the meaning of any
-  *   elements on Resource or DomainResource (including cannot change the meaning of modifierExtension itself).
+  *   - May be used to represent additional information that is not part of the basic definition of the resource and that modifies
+  *     the understanding of the element that contains it and/or the understanding of the containing element's descendants.
+  *     Usually modifier elements provide negation or qualification. To make the use of extensions safe and manageable, there is a
+  *     strict set of governance applied to the definition and use of extensions. Though any implementer is allowed to define an
+  *     extension, there is a set of requirements that SHALL be met as part of the definition of the extension. Applications
+  *     processing a resource are required to check for modifier extensions. Modifier extensions SHALL NOT change the meaning of
+  *     any elements on Resource or DomainResource (including cannot change the meaning of modifierExtension itself).
   * @param verificationStatus
-  *   \- The verification status to support the clinical status of the condition.
+  *   - The verification status to support the clinical status of the condition.
   * @param bodySite
-  *   \- The anatomical location where this condition manifests itself.
+  *   - The anatomical location where this condition manifests itself.
   * @param identifier
-  *   \- Business identifiers assigned to this condition by the performer or other systems which remain constant as the resource
-  *   is updated and propagates from server to server.
+  *   - Business identifiers assigned to this condition by the performer or other systems which remain constant as the resource is
+  *     updated and propagates from server to server.
   * @param stage
-  *   \- Clinical stage or grade of a condition. May include formal severity assessments.
+  *   - Clinical stage or grade of a condition. May include formal severity assessments.
   * @param evidence
-  *   \- Supporting evidence / manifestations that are the basis of the Condition's verification status, such as evidence that
-  *   confirmed or refuted the condition.
+  *   - Supporting evidence / manifestations that are the basis of the Condition's verification status, such as evidence that
+  *     confirmed or refuted the condition.
   */
 @POJOBoilerplate
 class UKCore_Condition(
