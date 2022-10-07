54e44085b5607b1e67f68277e0c4737313d319ce
Hugh Simpson <hugh.simpson@babylonhealth.com>
2022-10-07 07:49:25 +0100

    bump generator and re-run it. Standardises ukcore comment formatting

--- core/src/test/scala/com/babylonhealth/lit/core/ChoiceTest.scala
+++ core/src/test/scala/com/babylonhealth/lit/core/ChoiceTest.scala
@@ -10,18 +10,18 @@ import com.babylonhealth.lit.core.model.Extension
 class ChoiceTest extends AnyFreeSpec with Matchers {
   "can extract the value from a choice if it matches" in {
     val code: Extension.ValueChoice = choice("code": Code)
-    val extractedCode = code.as[Code]
+    val extractedCode               = code.as[Code]
     extractedCode shouldEqual Some("code": Code)
     val string: Extension.ValueChoice = choice("string")
-    val extractedString = string.as[String]
+    val extractedString               = string.as[String]
     extractedString shouldEqual Some("string")
   }
   "doesn't extract the value from a choice if doesn't match" in {
     val code: Extension.ValueChoice = choice("code": Code)
-    val unextractedCode = code.as[String]
+    val unextractedCode             = code.as[String]
     unextractedCode shouldEqual None
     val string: Extension.ValueChoice = choice("string")
-    val unextractedString = string.as[Code]
+    val unextractedString             = string.as[Code]
     unextractedString shouldEqual None
   }
 }
